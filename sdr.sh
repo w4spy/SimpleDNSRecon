639d473f04e69f3d3aa011aec796c406b37b925a
w4spy <40596946+w4spy@users.noreply.github.com>
2019-10-03 10:51:09 +0100

    Update sdr.sh

--- sdr.sh
+++ sdr.sh
@@ -6,18 +6,34 @@ echo '''
 └─┘┴┴ ┴┴  ┴─┘└─┘  ═╩╝╝╚╝╚═╝  ┴└─└─┘└─┘└─┘┘└┘  
                                             by waspy
 '''
-echo "[!] usage:./sdr.sh domain.any"
+display_usage() { 
+	echo "[!] Missing argument" 
+	echo "Usage: $0 domain " 
+	} 
+
+if [  $# -le 0 ] 
+	then 
+		display_usage
+		exit 1
+	fi 
 
 tempfile=$(mktemp)
-echo  "\n\033[0;32m[!]using amass...\n"
+
+echo -e "\n\033[0;32m[!] using amass...\n"
 amass enum -d $1 |tee -a $tempfile
-echo  "\n\033[0;32m[!]using crt.sh...\n"
+echo -e "\n\033[0;32m[!] using crt.sh...\n"
 curl -s https://crt.sh/?q=%.$1  | sed 's/<\/\?[^>]\+>//g' | grep $1 | tee -a $tempfile 
-echo  "\n\033[0;32m[!]using certspotter.com...\n"
+echo -e "\n\033[0;32m[!] using certspotter.com...\n"
 curl -s https://certspotter.com/api/v0/certs\?domain\=$1 | jq '.[].dns_names[]' | sed 's/\"//g' | sed 's/\*\.//g' | sort -u | tee -a $tempfile
-echo "\n\033[0;32m[!]http probing...\n"
+echo -e "\n\033[0;32m[!] using sublist3r...\n"
+sublist3r -d $1 | grep $1 | tee -a $tempfile 
+echo -e "\n\033[0;32m[!] http probing...\n"
 cat $tempfile | httprobe | grep $1 | sort -u | tee -a domains.out 
 rm $tempfile
-mkdir screenshot && cd screenshot
-/opt/gowitness -T 300 file --source domains.out
+mkdir -p sdrscreenshots && cd sdrscreenshots
+echo -e "\n\033[0;32m[!] taking screenshots using gowitness...\n"
+log=$(mktemp)
+/opt/gowitness -T 300 file --source ../domains.out 2> $log
 /opt/gowitness report generate
+grep 'error' $log | grep 'http' | tee error_gowitness.log
+rm $log
